4fc9ffeb0027276d6aaa0d43b5d7d9ed838d426c
Boniface Bassey <bonitusloveu@yahoo.com>
2016-02-12 16:23:36 +0100

    Latest sensor code

--- SensorNodeMain.cpp
+++ SensorNodeMain.cpp
@@ -2,13 +2,13 @@
 \file SensorNodeMain.c
 
 \DESCRIPTION:   
-       - This File is part of the wsnlab project
-       - It reads/manipulate platypus sensors and send over network
+       - This File is part of our wsnlab project
+       - Reads/manipulate platypus sensors and send over network
 
 \n Copyright (c) wsnlab                           
 \n Date   : 12.02.2016
-\n Author : Boniface Bassey
-
+\n Boniface Bassey
+\n 
 ******************************************************************************/
 
 #include <iostream>
@@ -33,7 +33,6 @@
 
 #define PORT 8880
 #define serverIPv6 "bbbb::fec2:deff:fe30:ad8e"
-//#define serverIPv6 "bbbb::fec2:deff:fe38:7234"
 
 #define LOOP_FOREVER while(true)
 
@@ -86,15 +85,15 @@ float zVal; //Current z Value
 
 //******************* Function Prototypes******************
 bool printSensorsID(int read, int target);
-bool getSensorsID();	//Get sensors ID
-void Initializations();	//Setup IMU, battery gauge, LDC
-void BatteryGauge();  	//Battery gauge
-void readIMU();		//IMU
-void getEnvSensors();	//Envornmental Sensors
-void printValues();	//Print Accelerometer
-void calibrateAccel();  //Calibrate Accelerometer
-bool checkMotion();	//Check for motion
-void INT_HANDLER(int sig);//Ctrl-C interrupt
+bool getSensorsID();		//Check for sensors availability
+void Initializations();		//Setup IMU, battery gauge
+void BatteryGauge();  		//Battery gauge
+void readIMU();				//IMU
+void getEnvSensors();		//Envornmental Sensors
+void printValues();			//Print Accelerometer after calibration
+void calibrateAccel();  	//Calibrate Accelerometer
+bool checkMotion();			//Check for motion
+void INT_HANDLER(int sig);	//Ctrl-C interrupt
 
 
 //*************Setup IMU, battery gauge, LDC***************
@@ -113,29 +112,28 @@ void Initializations()
 	}
 	
 	//Setup ldc
-	if (m_start_ldc){
-		m_ldc = new ldc_edison();	
-	}
+	//if (m_start_ldc){
+		//m_ldc = new ldc_edison();	
+	//}
 }
-
 
 //********************Get sensors ID**********************
 bool getSensorsID()
 {		
 	cout << "[IMU] ";
-	if (!printSensorsID(m_imu->getID(), m_imuID))
+	if (!printSensorsID(m_imu->getID(), m_imuID))				//IMU
 		success = false;
 	
 	cout << "[BAT] ";
-	if (!printSensorsID(m_batgauge->getVersion(), m_batID))
+	if (!printSensorsID(m_batgauge->getVersion(), m_batID))		//Battery gauge
 		success = false;	
 
 	cout << "[MAG] ";
-	if (!printSensorsID(m_imu->getMagID(), m_magID))	
+	if (!printSensorsID(m_imu->getMagID(), m_magID))			//Compass	
 		success = false;
 	
 	cout << "[ENV] ";
-	if (!printSensorsID(m_imu->getEnvID(), m_envID))
+	if (!printSensorsID(m_imu->getEnvID(), m_envID))			//Environmental
 		success = false;	
 
 	return success;
@@ -149,6 +147,7 @@ bool printSensorsID(int read, int target)
 		cout << " : Sensor available.\n";
 	else
 		cout << " : Sensor unavailable.\n";
+	
 	return read == target;	
 }
 
@@ -158,12 +157,11 @@ void BatteryGauge()
 {
 	if (m_start_bat)
 	{	
-		//cout << "\n";
-		cout << "Battery Voltage: " << m_batgauge->getVCell() << "V" << endl;
-		cout << "State of charge: " << m_batgauge->getSoC() << "%" << endl;
-		cout << "Alert Threshold: "<< m_batgauge->getAlertThreshold() << "%" << endl;
+		cout << "Battery Voltage: " << m_batgauge->getVCell() << "V" << endl;			//Battery voltage
+		cout << "State of charge: " << m_batgauge->getSoC() << "%" << endl;				//state of charge
+		cout << "Alert Threshold: " << m_batgauge->getAlertThreshold() << "%" << endl;	//Battery alert
 	
-		if (m_batgauge->getAlertStatus())
+		if (m_batgauge->getAlertStatus())		//Battery alert status
 			cout << "Alert is triggered" << endl;
 		else
 			cout << "Alert not triggered" << endl;
@@ -186,35 +184,35 @@ void readIMU()
 	float mx, my, mz;
        	m_imu->getCompassData(mx, my, mz);
 	
-	cout << "\r"
-	     << setw(12) << data[6] <<"\t"
-	     << setw(10) << data[0] << setw(10) << data[1] << setw(10) << data[2] <<"\t"
-	     << setw(10) << data[3] << setw(10) << data[4] << setw(10) << data[5] <<"\t"
-	     << setw(10) << mx      << setw(10) << my      << setw(10) << mz << std::flush; 
+	    cout << setw(12) << data[6] <<"\t"	//Temperature
+			 << setw(10) << data[0] << setw(10) << data[1] << setw(10) << data[2] <<"\t"	//Accelerometer
+			 << setw(10) << data[3] << setw(10) << data[4] << setw(10) << data[5] <<"\t"	//Gyroscope
+			 << setw(10) << mx      << setw(10) << my      << setw(10) << mz 	  << endl; 	//Compass
 }
 		
-
 //*******************Environmental Sensor********************
 void  getEnvSensors()
 {
 	if (m_start_env && m_start_imu)
 	{
 		float temp, pressure, humidity;		
+		
 		cout << setw(18) << "Temperature [DegC]" <<"\t"
-		     << setw(14) << "Pressure [hPa]" <<"\t"
-	     	     << setw(14) << "Humidity [%RH]" << endl;
+		     << setw(14) << "Pressure [hPa]" 	 <<"\t"
+	     	 << setw(14) << "Humidity [%RH]" 	 << endl;
    			
 		m_imu->getEnvData(temp, pressure, humidity);
 	
-		cout << "\r"
-		     << setw(18) << temp << "\t"
+		cout << setw(18) << temp 	 << "\t"
 		     << setw(14) << pressure << "\t"
-		     << setw(14) << humidity << "\t" << std::flush;		
+		     << setw(14) << humidity << "\t" << endl;		
 	}
 }
 
 
-
+//******************************************************************************
+// MAIN
+//******************************************************************************
 int main (int argc, char** argv)
 {		
 	int sock;
@@ -223,7 +221,7 @@ int main (int argc, char** argv)
 	char buffer[1024];
 	char addrbuf[INET6_ADDRSTRLEN];
 
-	/* create a DGRAM (UDP) socket in the INET6 (IPv6) protocol */
+	// create a DGRAM (UDP) socket in the INET6 (IPv6) protocol 
 	sock = socket(PF_INET6, SOCK_DGRAM, 0);
 
 	if (sock < 0) {
@@ -231,22 +229,23 @@ int main (int argc, char** argv)
 		exit(1);
 	}
 
-	/* create server address: where we want to send to */
-
-	/* clear it out */
+	// create server address: where we want to send to 
+	
+	// clear it out 
 	memset(&server_addr, 0, sizeof(server_addr));
 
-	/* it is an INET address */
+	// it is an INET address 
 	server_addr.sin6_family = AF_INET6;
 
-	/* the server IP address, in network byte order */
+	// the server IP address, in network byte order 
 	//inet_pton(AF_INET6, argv[1], &server_addr.sin6_addr);
 	inet_pton(AF_INET6, serverIPv6, &server_addr.sin6_addr);
 
-	/* the port we are going to send to, in network byte order */
-	//get the port number from the IP address
+	// the port we are going to send to, in network byte order 
+	// get the port number from the IP address
 	char *sep_ip; 
 	char last_segment[20];
+	
 	//get the last digits
 	printf("IPv6 address of the server: %s\n", argv[1]);
 	sep_ip = strtok(argv[1], "::");
@@ -276,11 +275,11 @@ int main (int argc, char** argv)
 // ******************// *******************// *****************
 	float temp, pressure, humidity;	
 	char sendbuffer[1024];
-	char ID_Test[10] = {0};
-	char ENV_H[10] = {0};	
-	char ENV_T[10] = {0};
-	char IMU_xyz[10] = {0};
-	char BAT_ALERT[12] = {0};
+	//char ID_Test[10] = {0};
+	//char ENV_H[10] = {0};	
+	//char ENV_T[10] = {0};
+	//char IMU_xyz[10] = {0};
+	//char BAT_ALERT[12] = {0};
 	//float BAT_Vol;
 	//int BAT_SoC;
 	//int BAT_A_T;
@@ -294,9 +293,11 @@ int main (int argc, char** argv)
 	printf("\n-------------------------------\n");
 	success = getSensorsID();
 	if (success){
-		strcpy(ID_Test, "ID_P");
+		//strcpy(ID_Test, "ID_P");
+		printf("Sensors ID Test...PASSED\n");
 	}else{ 
-		strcpy(ID_Test, "ID_F");
+		//strcpy(ID_Test, "ID_F");
+		printf("Some sensors ID Test...FAILED\n");
 	}
 	printf("\n-------------------------------\n");
 
@@ -326,28 +327,33 @@ int main (int argc, char** argv)
 			m_imu->getEnvData(temp, pressure, humidity);
 			
 			//Humidity Sensor
-			if (humidity > 40){
+			if (humidity > 45){	
 				break;
 			}
 			
 			// Once the accelerometer is calibrated - check for movement 
 			if(checkMotion()){
 				moveDetected = true;
-				strcpy(IMU_xyz, "VIB");
+				//strcpy(IMU_xyz, "VIB");
 				break;
 			} 
 			else{
-				strcpy(IMU_xyz, "NVIB");
+				//strcpy(IMU_xyz, "NVIB");
 				moveDetected = false;
 			}
 			 
 			//Temperature	
-			if(temp > 37){
+			if(temp > 35){
 				break;
 			}
 			usleep(100000);	//wait for 100ms
-		}while((humidity < 40)||(moveDetected == false));
+		}while((humidity < 45)||(moveDetected == false));
 
+	//Time of event:
+		curtime = time (NULL);	// Get the current time.  		
+  		loctime = localtime (&curtime);	// Convert it to local time representation. 
+  		strftime (time_buf, 12, "%I:%M:%S%p", loctime);
+		
 	//Battery Gauge		
 		//BAT_Vol = m_batgauge->getVCell();
 		//BAT_SoC = m_batgauge->getSoC();
@@ -355,72 +361,72 @@ int main (int argc, char** argv)
 		if (m_batgauge->getAlertStatus()){
 			//strcpy(BAT_ALERT, "BAT_ALRT");
 			strcpy(sendbuffer, "D");			
-			printf("Battery Alert\n");
+			printf("%s | Battery Alert\n",time_buf);
+			
 			//now send a datagram 
 			if (sendto(sock, sendbuffer, sizeof(sendbuffer), 0,(struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
 				perror("sendto failed");
 				exit(4);
-			}	
+			}
+		usleep(5000000);
 		}else{
-			strcpy(BAT_ALERT, "N_BAT_ALRT");
+			//strcpy(BAT_ALERT, "N_BAT_ALRT");
 		}
 			
 	//Humidity Sensor
-		if (humidity > 40){
+		if (humidity > 45){
 			//strcpy(ENV_H, "BLOW");
-			strcpy(sendbuffer, "B");			
-			printf("Blow\n");
+			strcpy(sendbuffer, "B");
+			printf("%s | BLOW\n",time_buf);
+			
 			//now send a datagram 
 			if (sendto(sock, sendbuffer, sizeof(sendbuffer), 0,(struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
 				perror("sendto failed");
 				exit(4);
-			}	
+			}
+		usleep(1000000);
 		}else{
-			strcpy(ENV_H, "NBLOW");
+			//strcpy(ENV_H, "NBLOW");
 		}
 		
 	//Accelerometer		
 		if(moveDetected == true){
-			strcpy(sendbuffer, "A");			
-			printf("Vibration\n");
-			
+			strcpy(sendbuffer, "A");
+			printf("%s | VIBRATION\n",time_buf);
+
 			//now send a datagram 
 			if (sendto(sock, sendbuffer, sizeof(sendbuffer), 0,(struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
 				perror("sendto failed");
 				exit(4);
 			}	
+		usleep(1000000);
 		}else{		
-			strcpy(IMU_xyz, "NVIB");
+			//strcpy(IMU_xyz, "NVIB");
 		}
 		
 	//Temperature	
-		if(temp > 37){
+		if(temp > 35){
 			//strcpy(ENV_T, "HOT");
-			strcpy(sendbuffer, "C");			
-			printf("Hot\n");
+			strcpy(sendbuffer, "C");
+			printf("%s | HOT\n",time_buf);
 			
 			//now send a datagram 
 			if (sendto(sock, sendbuffer, sizeof(sendbuffer), 0,(struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
 				perror("sendto failed");
 				exit(4);
 			}	
+		usleep(1000000);
 		}else{		
-			strcpy(ENV_T, "COLD");
+			//strcpy(ENV_T, "COLD");
 		}
 
-		// Get the current time. 
-/*  		curtime = time (NULL);
-
-  		// Convert it to local time representation. 
-  		loctime = localtime (&curtime);
-  		strftime (time_buf, 12, "%I:%M:%S%p", loctime);
 
 	//Prepare message buffer to send
-		sprintf(sendbuffer, "%s | %s | %.4fV | %d% | %d% | %s | %s | %s | %.4f |", 
-			time_buf, ID_Test, BAT_Vol, BAT_SoC, BAT_A_T, BAT_ALERT, ENV_H, IMU_xyz, temp);
-*/
+		//sprintf(sendbuffer, "%s | %s | %.4fV | %d% | %d% | %s | %s | %s | %.4f |", 
+		//	time_buf, ID_Test, BAT_Vol, BAT_SoC, BAT_A_T, BAT_ALERT, ENV_H, IMU_xyz, temp);
+
 		//sprintf(sendbuffer, "%s | %s | %s | %s |", BAT_ALERT, ENV_H, ENV_T, IMU_xyz);
-		// fputs (sendbuffer, stdout);
+		//fputs (sendbuffer, stdout);
 	
 	//now send a datagram 
 /*		if (sendto(sock, sendbuffer, sizeof(sendbuffer), 0,
@@ -434,18 +440,17 @@ int main (int argc, char** argv)
 		do{
 			//Environmental sensor--- Get humidity reading
 			m_imu->getEnvData(temp, pressure, humidity);			
+													
+			if (temp > 35)
+				printf("...Waiting for Temperature to cool down...\n");
 			
-			if(checkMotion()){
-				usleep(1000000);
-				break;
-			}
-			else if (temp > 33){
-				usleep(1000000);
-				break;
-			}
-				
-			usleep(2000000);
-		}while(humidity > 50);
+			if (humidity > 45)
+				printf("...Waiting for Blow to die out...\n");
+			
+			usleep(5000000);
+		}while((humidity > 45) || (temp > 35));
+		
+		printf("DONE...READY FOR NEXT OPERATION.\n");
 	}		 
  
 }
@@ -531,8 +536,8 @@ void calibrateAccel()
  		//Delay 10msec between readings
  		usleep(10000);
  	}
- 
  	//End of calibration.
+	
 	printf("Accelerometer is Calibrated.\n");
  	printValues();
  	calibrated = true;
@@ -540,7 +545,6 @@ void calibrateAccel()
 
 
 // Prints the Sensor limits identified during Accelerometer calibration.
-// Prints to the Serial monitor.
 void printValues()
 {
  	printf("xMin= %f\t", xMin);
